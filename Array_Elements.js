2a8142c65f060b575dc15e36ccbacbd0ca956bbc
Atom <80907317+D3EPAK@users.noreply.github.com>
2021-03-25 20:18:23 +0530

    Create Array_Elements.js

---
+++ Array_Elements.js
@@ -0,0 +1,7 @@
+// Printing the elements of a given array using function
+function arrayElementd(){
+	for(var i = 0; i<= arr.length-1; i++){
+		console.log(arr[i])
+	}
+}
+arrayElementd(arr = [12, 23, 34, 32])
